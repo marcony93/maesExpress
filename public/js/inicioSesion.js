34e06ccfc7493fb9a304497f177bbcabb9a66bde
marcony meza <marcony meza>
2017-02-09 14:56:13 -0600

    funcion de error inicio sesion

--- public/js/inicioSesion.js
+++ public/js/inicioSesion.js
@@ -5,11 +5,10 @@ function iniciarSesion(){
     var contrasenia = document.getElementById('password').value;
 
     var req = {User:"'"+usuario+"'",password:contrasenia};
-    console.log(req)
     window.http('http://localhost:8555/api/iniciarSesion',req,function(res){  
         var data = JSON.parse(res)
         if(data.confirmacion == true)   window.location.replace(window.location.origin + '/user');
-        
+        else $('#errorMessage').modal('show');
 })
 }
 
